caf1d1edb9a002e030c55ea8f0dd9efcf49c137f
Bongo Cat Studios <123309704+FoxyLemonslayer@users.noreply.github.com>
2024-07-18 19:43:48 +0200

    Update Beebo_Browser.py

--- Beebo_Browser.py
+++ Beebo_Browser.py
@@ -4,7 +4,6 @@ from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 import sys
 import sqlite3
-
 class MyWebBrowser(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MyWebBrowser, self).__init__(*args, **kwargs)
@@ -80,27 +79,22 @@ class MyWebBrowser(QMainWindow):
         self.setStyleSheet(self.default_style)
         self.incognito = False
         self.init_db()
-
-        # URL Bar
+        #URL Bar
         self.url_bar = QLineEdit()
         self.url_bar.setPlaceholderText("Enter URL or search term...")
         self.url_bar.returnPressed.connect(self.navigate_to_url)
-
-        # Browser Tabs
+        #Browser Tabs
         self.tabs = QTabWidget()
         self.tabs.setTabsClosable(True)
         self.tabs.tabCloseRequested.connect(self.close_tab)
         self.tabs.currentChanged.connect(self.current_tab_changed)
         self.setCentralWidget(self.tabs)
-        
-        # Add the first tab
+        #Add the first tab
         self.add_new_tab(QUrl('http://google.com'), 'New Tab')
-
-        # Layouts
+        #Layouts
         self.layout = QVBoxLayout()
         self.horizontal = QHBoxLayout()
-
-        # Buttons
+        #Buttons
         self.back_btn = QPushButton('<')
         self.back_btn.clicked.connect(self.navigate_back)
         self.forward_btn = QPushButton('>')
@@ -127,8 +121,7 @@ class MyWebBrowser(QMainWindow):
         self.go_btn = QPushButton('Go')
         self.go_btn.setObjectName('go_btn')
         self.go_btn.clicked.connect(self.navigate_to_url)
-
-        # Add buttons and URL bar to horizontal layout
+        #Add buttons and URL bar to horizontal layout
         self.horizontal.addWidget(self.back_btn)
         self.horizontal.addWidget(self.forward_btn)
         self.horizontal.addWidget(self.refresh_btn)
@@ -142,8 +135,7 @@ class MyWebBrowser(QMainWindow):
         self.horizontal.addWidget(self.downloads_btn)
         self.horizontal.addWidget(self.incognito_btn)
         self.horizontal.addWidget(self.new_tab_btn)
-
-        # Progress Bar
+        #Progress Bar
         self.progress_bar = QProgressBar()
         self.progress_bar.setMaximumHeight(20)
         self.progress_bar.setVisible(False)
@@ -158,18 +150,14 @@ class MyWebBrowser(QMainWindow):
                 width: 20px;
             }
         """)
-
-        # Add layouts and widgets
+        #Add layouts and widgets
         self.layout.addLayout(self.horizontal)
         self.layout.addWidget(self.progress_bar)
         self.layout.addWidget(self.tabs)
-
         container = QWidget()
         container.setLayout(self.layout)
         self.setCentralWidget(container)
-
         self.show()
-
     def init_db(self):
         self.conn = sqlite3.connect('browser_data.db')
         self.cursor = self.conn.cursor()
@@ -178,59 +166,45 @@ class MyWebBrowser(QMainWindow):
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS bookmarks
                                (id INTEGER PRIMARY KEY, url TEXT)''')
         self.conn.commit()
-
     def add_new_tab(self, qurl=None, label="Blank"):
         browser = QWebEngineView()
         browser.setUrl(QUrl('http://google.com') if qurl is None else qurl)
         browser.urlChanged.connect(self.update_url)
         browser.loadProgress.connect(self.update_progress)
         browser.loadFinished.connect(self.add_to_history)
-        
         i = self.tabs.addTab(browser, label)
         self.tabs.setCurrentIndex(i)
         browser.page().titleChanged.connect(lambda title, browser=browser: self.update_tab_title(browser, title))
-
     def update_tab_title(self, browser, title):
         index = self.tabs.indexOf(browser)
         if index != -1:
             self.tabs.setTabText(index, title)
-
     def current_browser(self):
         return self.tabs.currentWidget()
-
     def navigate_back(self):
         self.current_browser().back()
-
     def navigate_forward(self):
         self.current_browser().forward()
-
     def refresh_page(self):
         self.current_browser().reload()
-
     def stop_loading(self):
         self.current_browser().stop()
-
     def navigate_to_url(self):
         url = self.url_bar.text()
         if not url.startswith("http"):
             url = "https://www.google.com/search?q=" + url
         self.current_browser().setUrl(QUrl(url))
-
     def navigate_home(self):
         self.current_browser().setUrl(QUrl('http://google.com'))
-
     def new_tab(self):
         self.add_new_tab(QUrl('http://google.com'), 'New Tab')
-
     def close_tab(self, index):
         if self.tabs.count() < 2:
             return
         self.tabs.removeTab(index)
-
     def current_tab_changed(self, index):
         qurl = self.current_browser().url()
         self.update_url(qurl)
-
     def toggle_incognito(self):
         self.incognito = not self.incognito
         if self.incognito:
@@ -241,22 +215,18 @@ class MyWebBrowser(QMainWindow):
             self.incognito_btn.setChecked(False)
             self.setWindowTitle('Lunar Browser')
             self.setStyleSheet(self.default_style)
-
     def update_url(self, q):
         self.url_bar.setText(q.toString())
-
     def update_progress(self, progress):
         self.progress_bar.setVisible(True)
         self.progress_bar.setValue(progress)
         if progress == 100:
             self.progress_bar.setVisible(False)
-
     def add_to_history(self):
         if not self.incognito:
             url = self.current_browser().url().toString()
             self.cursor.execute('INSERT INTO history (url) VALUES (?)', (url,))
             self.conn.commit()
-
     def show_history(self):
         self.cursor.execute('SELECT url FROM history')
         history_data = self.cursor.fetchall()
@@ -272,13 +242,11 @@ class MyWebBrowser(QMainWindow):
         history_layout.addWidget(close_button)
         history_dialog.setLayout(history_layout)
         history_dialog.exec_()
-
     def add_bookmark(self):
         url = self.current_browser().url().toString()
         self.cursor.execute('INSERT INTO bookmarks (url) VALUES (?)', (url,))
         self.conn.commit()
         QMessageBox.information(self, "Bookmark Added", f"Bookmarked {url}")
-
     def show_bookmarks(self):
         self.cursor.execute('SELECT url FROM bookmarks')
         bookmarks_data = self.cursor.fetchall()
@@ -295,10 +263,8 @@ class MyWebBrowser(QMainWindow):
         bookmarks_layout.addWidget(close_button)
         bookmarks_dialog.setLayout(bookmarks_layout)
         bookmarks_dialog.exec_()
-
     def load_bookmarked_page(self, item):
         self.current_browser().setUrl(QUrl(item.text()))
-
     def show_downloads(self):
         downloads_dialog = QDialog(self)
         downloads_dialog.setWindowTitle("Downloads")
@@ -312,7 +278,6 @@ class MyWebBrowser(QMainWindow):
         downloads_layout.addWidget(close_button)
         downloads_dialog.setLayout(downloads_layout)
         downloads_dialog.exec_()
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle('Fusion')
